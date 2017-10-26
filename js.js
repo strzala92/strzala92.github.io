23b11c1a7c23bc12f9714d0b06f3fc940f060a03
Sebastian Strzelecki <strzeleckis20@gmail.com>
2017-10-26 22:41:46 +0200

    Sortowanie

---
+++ js.js
@@ -0,0 +1,19 @@
+var tablicaDoPosortowania = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 3];
+function sortownaie(tablica){
+for (var i = 1; i < tablica.length; i++)
+    {
+        var pomocnicza=tablica[i];
+        var j = i
+        for(j=i; tablica[j-1]>pomocnicza; j--)
+            {
+                tablica[j]=tablica[j-1];
+            }
+        
+            tablica[j]=pomocnicza;
+        
+    
+        
+    }
+   return tablica;
+}
+console.log(sortownaie(tablicaDoPosortowania));

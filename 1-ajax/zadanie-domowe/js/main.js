87c5c7912ab9885f65613ea91c9c4ea282577b19
Sebastian Strzelecki <strzeleckis20@gmail.com>
2017-11-03 13:00:35 +0100

    poprawa

--- 1-ajax/zadanie-domowe/js/main.js
+++ 1-ajax/zadanie-domowe/js/main.js
@@ -2,10 +2,10 @@ $(function () {
     'use strict';
 
     $("button").click(function () {
+            $('body').append('<div id="dane"></div>');
 
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-            
-            $('#dane-programisty').html(
+            $('#dane').html(
            "<p>Firma: "+data.firma+"</p>"+
            "<p>Imie: "+data.imie+"</p>"+
            "<p>Nazwisko: "+data.nazwisko+"</p>"+
